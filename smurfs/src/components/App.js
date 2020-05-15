783be5b425d3ff1fb03f7059cc3d4f7c0bd995d7
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-05-15 10:24:03 -0700

    added contexts and three more smurf compoenents

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,46 @@
-import React, { Component } from "react";
+import React, { useState, useEffect  } from "react";
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
+
+// imports
+import axios from 'axios'
+import SmurfForm from './components/SmurfForm';
+import SmurfList from './components/SmurfList';
+import { SmurfContext } from './contexts/SmurfContext';
+
+// import styles 
+import styled from 'styled-components'
+
+const Village = styled.div`
+  width: 400px;
+  margin: auto;
+`
+
+const App = () => {
+  const [smurfs, setSmurfs] = useState([])
+  const addSmurf = newSmurf => {
+    setSmurfs([...SmurfList, newSmurf]);
   }
-}
+  useEffect(() => {
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(res =>{
+        console.log(res.data)
+        setSmurfs(res.data)
+      })
+  }, [])
+  
+  
+      return (
+        <SmurfContext.Provider value={{addSmurf, smurfs}}>
+        <Village>
+        
+        <SmurfList/>
+        <SmurfForm/>
+  
+        </Village>
+        </SmurfContext.Provider>
+      );
+    
+  }
 
 export default App;
